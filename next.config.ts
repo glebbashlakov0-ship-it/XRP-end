e05c4bb7a7fb1f317abc79deb287411685b0f121
glebbashlakov0-ship-it <glebbashlakov0@gmail.com>
2026-01-12 21:36:17 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import type { NextConfig } from "next";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  outputFileTracingRoot: __dirname
 };
 
 export default nextConfig;
